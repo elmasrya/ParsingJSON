980a666bcd2974058906af3a43e99d9c51231201
elmasrya <andrewelmasry@gmail.com>
2016-01-13 00:06:11 -0500

    turn in assignment

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 ##Notes:
 - JSON is a light weight data-integration
 - We can capture data from one program and integrate it in another program
+- Aggregate data from different Sources. 
+
+ 	> Example: Delta App gets data from flight times, schedules, ticket number, flight status,  bank account etc.
+
+- JSON is built into the Android platform
+ 	
--- app/src/main/java/com/testing/parsingjson/MainActivity.java
+++ app/src/main/java/com/testing/parsingjson/MainActivity.java
@@ -1,52 +1,104 @@
 package com.testing.parsingjson;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView tvData;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        tvData = (TextView)findViewById(R.id.text_holder);
+
+
+                new JSONTask().execute("http://de-coding-test.s3.amazonaws.com/books.json");
+
+    }
+
+    public class JSONTask extends AsyncTask<String, String, String> {
+        @Override
+        protected String doInBackground(String...params) {
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+
+            try {
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                InputStream stream = connection.getInputStream();
+
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuffer buffer = new StringBuffer();
+
+                String line= "";
+                while ((line=reader.readLine()) !=null) {
+                    buffer.append(line);
+                }
+                String totalJSON = buffer.toString();
+
+                try {
+                    JSONArray parentArray = new JSONArray(totalJSON);
+
+                        JSONObject jsonObject = parentArray.getJSONObject(0);
+
+
+                        String title = jsonObject.getString("title");
+                        String imageURL = jsonObject.getString("imageURL");
+
+
+
+
+                    return buffer.toString();
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+
+                }
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch(IOException e) {
+                    e.printStackTrace();
+
+                }
             }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            return null;
         }
 
-        return super.onOptionsItemSelected(item);
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            tvData.setText(result);
+        }
     }
+
 }
